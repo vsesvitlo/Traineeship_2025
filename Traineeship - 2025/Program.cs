530c2395d7701ca47bfd355cfc27d7ca1e96611f
Svitlana Frolova <sfrol91@gmail.com>
2024-12-29 12:56:37 +0100

    Add project files.

---
+++ Traineeship - 2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Traineeship___2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
