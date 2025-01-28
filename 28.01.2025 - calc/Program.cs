fc6c20826cdfec1f2c3074d276a8aa48a0edb876
Svitlana Frolova <sfrol91@gmail.com>
2025-01-28 12:17:52 +0100

    Delegates

---
+++ 28.01.2025 - calc/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+internal partial class Program
+{
+    delegate int Calc (int x, int y);
+    static List<int> ts = new List<int>();
+    private static void Main(string[] args)
+    {
+        Calc first = Sum;
+       
+        first += Min;
+        first += Mult;
+        first += Min;
+        first += Min;
+        first += Sum;
+        first += Min;
+        first += Div;
+        first -= Min;
+        first -= Min;
+        first.Invoke(6, 2);
+        foreach (int x in ts)
+        {
+            Console.WriteLine (x);
+        }
+        
+      
+
+    }
+    public static int Sum(int a, int b)
+    {
+       int result = a + b;
+        ts.Add(result);
+        return result;
+    }
+    public static int Mult(int a, int b)
+    {
+        int result = a * b;
+        ts.Add(result);
+        return result;
+    }
+    public static int Min(int a, int b)
+    {
+        int result = a - b;
+        ts.Add(result);
+        return result;
+    }
+    public static int Div(int a, int b)
+    {
+        int result = a / b;
+        ts.Add(result);
+        return result;
+    }
+}
