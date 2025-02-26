40cdfed1d6f2e053c96bfbc0d046eae43a4fea3b
Svitlana Frolova <sfrol91@gmail.com>
2025-02-26 16:29:54 +0100

    Lambda-tasks

---
+++ 26.02 - 2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Завдання 2: Використання Func<> і Action<>
+Створіть метод ProcessOperation, який приймає делегат Func<int, int, int> 
+і два числа.
+Використайте його для виконання різних операцій (додавання, множення тощо) 
+за допомогою лямбда-виразів.
+Микола Оліфірович, [26/02/2025 13:24]
+стоврити екземпляр стандартного делегату Funk<>
+
+Микола Оліфірович, [26/02/2025 13:24]
+який буде містити в собі функцію яка приймає один int і повертає один int
+ */
+namespace _26._02___2
+{
+    internal class Program
+    {
+        
+
+        static void ProcessOperation(Func <int, int, int, int> func, int d, int e, int f)
+        {
+            Console.WriteLine(func(d, e, f));
+        }
+
+
+        static void Main(string[] args)
+        {
+            ProcessOperation((x, y, z) => x + y + z, 5, 3, 8);
+            ProcessOperation((x, y, z) => x * y * z, 2, 10, 4);
+
+            Func<int, int> example = (int s) => s + 1;
+            Console.WriteLine(example.Invoke(9));
+        }
+    }
+}
---
+++ 26.02/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Створіть делегат Operation, який приймає два числа і повертає число.
+ * Напишіть методи Add (додавання) і Multiply (множення).
+ * Використайте делегат для виклику цих методів.
+*/
+
+namespace _26._02
+{
+    internal class Program
+    {
+        delegate int Operation(int a, int b);
+
+        static int Multiply(int e, int f)
+        {
+            int result = e * f;
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Operation Add = (int x, int y) => x + y;
+            Operation Mult = Multiply;
+            Console.WriteLine(Mult(7,8));
+            Console.WriteLine(Mult.Invoke(3,5));
+        }
+    }
+}
