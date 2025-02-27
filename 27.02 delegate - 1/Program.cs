1e97bbd2fc3995aa1414bc403914e9f37298acb0
Svitlana Frolova <sfrol91@gmail.com>
2025-02-27 18:55:49 +0100

    in process

---
+++ 27.02 delegate - 1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Завдання 3: Фільтрація списку через делегат
+Напишіть метод FilterList, який приймає список чисел та делегат Predicate<int>.
+Метод має повертати новий список, який містить тільки ті числа,
+для яких делегат повертає true.
+Використайте цей метод для фільтрації парних чисел зі списку.*/
+
+using System.Xml.Linq;
+
+namespace _27._02_delegate___1
+{
+    internal class Program
+    {
+        delegate int Predicate(int a);
+        static void FilterList(List<int> func, Predicate pred)
+        {
+           
+            if (pred == true)
+               
+
+            return 
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
