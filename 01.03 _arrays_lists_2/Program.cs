f2df191b74e1b83ebd0f87ce12c698d09bc5ddaf
Svitlana Frolova <sfrol91@gmail.com>
2025-03-01 20:17:10 +0100

    Solving task List to array

---
+++ 01.03 _arrays_lists_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/*2. Створи список рядків, що містить три довільні слова. 
+ * Перетвори цей список у масив,
+ * після чого виведи елементи масиву в зворотному порядку.*/
+namespace _01._03__arrays_lists_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = new List<string>();
+            words.AddRange(new string[] { "adfg", "Ryiu;j", "k"});
+            words.ToArray();
+            for (int i = words.Count - 1; i >= 0; i--)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
