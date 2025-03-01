d87a34bd7260d57971c6f7517c3ccdc200fde1bb
Svitlana Frolova <sfrol91@gmail.com>
2025-03-01 22:26:37 +0100

    Solving the task with max and min values

---
+++ 01.03 _arrays_lists_5/Program.cs
@@ -0,0 +1,15 @@
+﻿/*5. Напиши код, який знаходить максимальне та 
+ * мінімальне значення у списку чисел.*/
+namespace _01._03__arrays_lists_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> list = new List<int>();
+           list.AddRange(new int [] { 9, 8, 444, 1 });
+           Console.WriteLine(list.Min());
+           Console.WriteLine(list.Max());
+        }
+    }
+}
