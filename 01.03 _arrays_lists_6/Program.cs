351c37ca65bdfd883cdf4e41ffa668fcfaaa0fe0
Svitlana Frolova <sfrol91@gmail.com>
2025-03-02 12:41:37 +0100

    Solving the task with adding element to arr through List

---
+++ 01.03 _arrays_lists_6/Program.cs
@@ -0,0 +1,24 @@
+﻿/*6. Реалізуй програму, яка додає до масиву новий елемент, 
+ * не створюючи новий масив вручну (використай List<T> для зручності).*/
+namespace _01._03__arrays_lists_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int a = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                a += 1;
+                arr[i] = a;
+                //Console.WriteLine(a);
+            }
+            List<int>list = arr.ToList();
+            //Console.WriteLine(arr.GetType());
+            list.Add(111);
+            list.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine(list.GetType());
+        }
+    }
+}
