a0dea746ebcd1fdf264e7e2b6cd2c90c291e2935
Svitlana Frolova <sfrol91@gmail.com>
2025-03-12 14:59:58 +0100

    Solving MoveZeroes

---
+++ 12.03 - loop_00/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _12._03___loop_00
+{
+    internal class Program
+    {
+        public static int[] MoveZeroes(int[] nums)
+        {
+            int size = nums.Length;
+            int[] nums2 = new int[size];
+            for (int i = 0, j = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    nums2[j] = nums[i];
+                    j++;
+                }
+                
+            }
+            return nums2;
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 0, 3, 4, 5, 0 };
+            int[] nums2 = MoveZeroes(nums);
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                Console.WriteLine(nums2[i]);
+            }
+
+        }
+    }
+}
---
+++ 12.03 - zeroos massive sorting/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _12._03___zeroos_massive_sorting
+{
+    internal class Program
+    {
+        public static int[] MoveZeroes(int[] nums)
+        {
+            int size = nums.Length;
+            int[] nums2 = new int[size];
+            for (int i = 0, j = nums.Length - 1; i < nums.Length && j >= 0; i++, j--)
+            {
+                if (nums[i] != 0)
+                {
+                    nums2[i] = nums[i];
+                }
+                else
+                {
+                    nums2[j] = nums[i];
+                    i++;
+                }
+            }
+           return nums2;
+        }
+            static void Main(string[] args)
+            {
+            int[] nums = { 1, 2, 0, 3, 4, 5, 0 };
+            int[] nums2 = MoveZeroes(nums);
+            for (int i = 0; i < nums2.Length; i++) {
+                Console.WriteLine(nums2[i]);
+            }
+
+        }
+    }
+}
