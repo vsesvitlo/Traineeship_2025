b92215025f5a6d6c447a9a388e9d375ece394107
Svitlana Frolova <sfrol91@gmail.com>
2025-03-14 14:46:27 +0100

    Solved task- loop_00

---
+++ 12.03 - loop_00_ 2_version/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _12._03___loop_00__2_version
+{
+    internal class Program
+    {
+        public static int[] MoveZeroes(int[] nums)
+        {
+            for (int i = 0, j = 1; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    if (nums[j] != 0)
+                    {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        j++;
+                    }
+                    else
+                    {
+                       while (nums[j] == 0 && j < nums.Length - 1)
+                        {
+                            j++;
+                            
+                        }
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+
+                    }
+
+                }
+                
+            }
+            return nums;
+        }
+         static void Main(string[] args)
+        {
+            int[] nums = { 0, 1, 0, 3, 12 };
+            int[] nums2 = MoveZeroes(nums);
+            for (int i = 0; i < nums2.Length; i++)
+            {
+                Console.WriteLine(nums2[i]);
+            }
+        }
+    }
+}
