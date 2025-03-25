75593ae3b4867aa363c44a0f033224d2185a08fb
Svitlana Frolova <sfrol91@gmail.com>
2025-03-25 09:52:02 +0100

    Solving task 12+13

---
+++ 25.03 - 12/Program.cs
@@ -0,0 +1,22 @@
+﻿/*12. Напиши програму, яка зчитує double число від користувача, 
+конвертує його у int (відкидаючи дробову частину), 
+а потім виводить обидва значення.
+
+*/namespace _25._03___12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double additions = 0;
+            int main = 0;
+            Console.WriteLine("Enter the number, please");
+            double item = double.Parse(Console.ReadLine());
+            main = (int)Math.Floor(item);
+            double forADD = Convert.ToDouble(main);
+            additions = item - forADD;
+            Console.WriteLine(additions);
+            Console.WriteLine(main);
+        }
+    }
+}
---
+++ 25.03 - 13/Program.cs
@@ -0,0 +1,24 @@
+﻿/*13. Створи список цілих чисел, перетвори його у список рядків,
+а потім виведи всі елементи списку, об'єднавши їх в один рядок через кому.
+*/
+namespace _25._03___13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> list = new List<int> { 1, 3, 7, 9};
+           List<string> list2 = new List<string> { };
+            for (int i = 0; i < list.Count; i++) {
+                list2.Add(Convert.ToString(list[i]));
+            }
+            string result = "";
+            for (int i = 0; i < list2.Count; i++) { 
+                result += list2[i] + ", ";
+            }
+            result = result.Substring(0, result.Length - 2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
