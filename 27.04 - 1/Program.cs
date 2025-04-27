337d455995fdac17c3e6288d2b9bd2f5c5aa6957
Svitlana Frolova <sfrol91@gmail.com>
2025-04-27 12:29:55 +0200

    Solving the task: get, set properties

---
+++ 27.04 - 1/Program.cs
@@ -0,0 +1,68 @@
+﻿/*Завдання 1: Автоматичні властивості
+Створи клас Book з автоматичними властивостями:
+Title (рядок)
+Author (рядок)
+Pages (ціле число)
+Створи об'єкт і виведи інформацію про книгу.*/
+
+namespace _27._04___1
+{
+    public class Book
+    {
+       public string title;
+       public string author;
+       public int pages;
+       /*public Book(string title, string author, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+        }*/
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        public int Pages
+        {
+            get
+            {  
+                return pages;
+            }
+            set
+            {
+                pages = value;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book FirstExample = new Book();
+            FirstExample.title = "Hygge";
+            FirstExample.author = "Meik Wiking";
+            FirstExample.pages = 285;
+            Console.WriteLine(FirstExample.Author);
+            Console.WriteLine(FirstExample.Title);
+            Console.WriteLine(FirstExample.Pages);
+        }
+    }
+}
