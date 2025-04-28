7a08159847458bc41bc41f8d1f0d138b5dc8a0b6
Svitlana Frolova <sfrol91@gmail.com>
2025-04-28 19:21:20 +0200

    Solving the task with Area Circle, using readonly, get, set

--- 27.04 - 4/Program.cs
+++ 27.04 - 4/Program.cs
@@ -2,13 +2,56 @@
 Створи клас Circle з властивістю Radius і тільки 
 для читання властивістю Area,
 яка обчислює площу кола.*/
+using _27._04___4;
+
 namespace _27._04___4
 {
+    /*private readonly int MyVal = 5;
+
+    public int MyProp { get { return MyVal; } }
+    */
+    public class Circle
+    {
+        private readonly double area;
+        public double radius;
+        public Circle(double radius)
+        {
+           // this.area = Math.PI * Math.Pow(radius, 2);
+            this.Radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return Math.PI * Math.Pow(radius, 2);
+            }
+            set
+            {
+                Area = Math.PI * Math.Pow(radius, 2);
+            }
+        }
+
+    }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        Circle circle = new Circle(3.5);
+        Console.WriteLine(circle.Area);
         }
     }
-}
+
+
