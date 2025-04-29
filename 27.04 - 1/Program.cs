bf067bc6bb346aa9cfce5b6a98be61bb7031dd5a
Svitlana Frolova <sfrol91@gmail.com>
2025-04-29 11:30:30 +0200

    Auto propetries

--- 27.04 - 1/Program.cs
+++ 27.04 - 1/Program.cs
@@ -9,57 +9,19 @@ namespace _27._04___1
 {
     public class Book
     {
-       public string title;
-       public string author;
-       public int pages;
-       /*public Book(string title, string author, int pages)
-        {
-            this.title = title;
-            this.author = author;
-            this.pages = pages;
-        }*/
-        public string Title
-        {
-            get
-            {
-                return title;
-            }
-            set
-            {
-                title = value;
-            }
-        }
-        public string Author
-        {
-            get
-            {
-                return author;
-            }
-            set
-            {
-                author = value;
-            }
-        }
-        public int Pages
-        {
-            get
-            {  
-                return pages;
-            }
-            set
-            {
-                pages = value;
-            }
-        }
+       public string Title{ get; set;}
+       public string Author{ get; set;}
+       public int Pages{get; set;}
+        
     }
     internal class Program
     {
         static void Main(string[] args)
         {
             Book FirstExample = new Book();
-            FirstExample.title = "Hygge";
-            FirstExample.author = "Meik Wiking";
-            FirstExample.pages = 285;
+            FirstExample.Title = "Hygge";
+            FirstExample.Author = "Meik Wiking";
+            FirstExample.Pages = 285;
             Console.WriteLine("Book is written by: " + FirstExample.Author + ", named " +
                 FirstExample.Title + " and has " + FirstExample.Pages + " pages.");
         }
