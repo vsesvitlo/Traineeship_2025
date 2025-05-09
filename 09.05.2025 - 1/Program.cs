09d8d758a65ddd9f372de54ee7efa819a5408ae8
Svitlana Frolova <sfrol91@gmail.com>
2025-05-09 08:48:29 +0200

    1 task

---
+++ 09.05.2025 - 1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*1: Логування викликів функції
+Створи декоратор log_calls, який виводить повідомлення 
+перед і після виклику функції з її назвою.*/
+
+namespace _09._05._2025___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
