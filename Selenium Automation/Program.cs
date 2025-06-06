7225fe61f21f0c3042ab2c6e886abe0476faa139
Svitlana Frolova <sfrol91@gmail.com>
2025-06-06 16:56:10 +0200

    task from courses Selenium automation

---
+++ Selenium Automation/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_Automation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        IWebDriver driver = new ChromeDriver();
+    
+        driver.Navigate().GoToUrl("https://www.google.com");
+        driver.Manage().Window.Maximize();
+
+            IWebElement searchbox = driver.FindElement(By.Id("input"));
+
+            searchbox.SendKeys("This is...");
+
+            driver.Quit();
+        }
+    }
+}
